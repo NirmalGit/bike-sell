3c71059f38a8113b41a6cbc012405c3f2d6e7729
NirmalGit <nirsha2007@gmail.com>
2022-07-08 03:00:04 +0530

    login changes

--- libs/auth/src/lib/container/login/login.component.ts
+++ libs/auth/src/lib/container/login/login.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log()
+  }
+
+  login(authentic: any) {
+    console.log(authentic);
+  }
 }
