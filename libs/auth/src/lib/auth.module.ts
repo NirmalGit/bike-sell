697e955cada767de8cc73733ef4e172547d7f574
NirmalGit <nirsha2007@gmail.com>
2022-07-27 08:40:36 +0530

    feat: add material components

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -4,13 +4,14 @@ import { RouterModule, Route } from '@angular/router';
 import { LoginComponent } from './container/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from '@bike-sell/material';
 
 export const authRoutes: Route[] = [
   { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterModule, HttpClientModule, MaterialModule],
   declarations: [LoginComponent, LoginFormComponent],
 })
 export class AuthModule { }
